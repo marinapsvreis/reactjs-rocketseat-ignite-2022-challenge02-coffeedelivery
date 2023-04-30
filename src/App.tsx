88f1cc9fa3e44545986556b48108484ea9b64966
Marina Portugal <marinapsvreis@gmail.com>
2023-04-30 17:46:18 -0300

    :truck: React router dom configurado com as telas previstas até aqui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
-import { Home } from './pages/Home'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+import { GlobalStyle } from './styles/global'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
-  return <Home />
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
